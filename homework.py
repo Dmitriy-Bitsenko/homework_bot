ca940f6b0246c676dc9a29ca88a6367c456d6229
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2024-06-25 17:17:29 +0500

    after flake8

--- homework.py
+++ homework.py
@@ -146,47 +146,42 @@ def main():
 
     last_error_message = None
 
-    try:
-        while True:
-            try:
-                response = get_api_answer(timestamp)
-                if not response:
-                    logger.error("Ошибка запроса к эндпоинту: %s.")
+    while True:
+        try:
+            response = get_api_answer(timestamp)
+            if not response:
+                logger.error("Ошибка запроса к эндпоинту: %s.")
 
-                if HTTPStatus.OK:
-                    pass
-                else:
-                    logger.error(
-                        "Эндпоинт %s недоступен, статус код: %s",
-                        ENDPOINT, response.status_code
-                    )
+            if HTTPStatus.OK:
+                pass
+            else:
+                logger.error("Эндпоинт %s недоступен, статус код: %s",
+                             ENDPOINT, response.status_code)
 
-                homeworks = check_response(response)
-                if homeworks:
-                    message = parse_status(homeworks[0])
-                    send_message(bot, message)
-                if ("homework_name" not in homeworks
-                        or "status" not in homeworks):
-                    logger.error("Отсутствуют ключи homework_name или"
-                                 "status в ответе API")
-                homework_status = parse_status(["status"])
-                if homework_status not in HOMEWORK_VERDICTS:
-                    logger.error("Неизвестный статус домашней работы: %s",
-                                 homework_status)
-                else:
-                    logger.debug("Отсутствие новых статусов в ответе API.")
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
+                send_message(bot, message)
+            if ("homework_name" not in homeworks
+                    or "status" not in homeworks):
+                logger.error("Отсутствуют ключи homework_name или"
+                             "status в ответе API")
+            homework_status = parse_status(["status"])
+            if homework_status not in HOMEWORK_VERDICTS:
+                logger.error("Неизвестный статус домашней работы: %s",
+                             homework_status)
+            else:
+                logger.debug("Отсутствие новых статусов в ответе API.")
 
-                timestamp = response.get("current_date", timestamp)
-            except Exception as error:
-                message = f"Сбой в работе программы: {error}."
-                logger.error(message, exc_info=True)
-                if last_error_message != message:
-                    send_message(bot, message)
-                    last_error_message = message
-            finally:
-                time.sleep(RETRY_PERIOD)
-    except KeyboardInterrupt:
-        logger.info("Программа остановлена пользователем.")
+            timestamp = response.get("current_date", timestamp)
+        except Exception as error:
+            message = f"Сбой в работе программы: {error}."
+            logger.error(message, exc_info=True)
+            if last_error_message != message:
+                send_message(bot, message)
+                last_error_message = message
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == "__main__":
